74f0755a5571d10e25b3f8ffceb71b27f367614c
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-14 18:49:36 -0700

    commit tree f19b629b0538cc9289a450b87292e24730451f85

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,35 @@
-class Person
+# class Person
+#     def name
+#       @name
+#     end
 
-end
+#     def name=(name)
+#       @name = name
+#     end
+
+#     def job 
+#       @job
+#     end
+
+#     def job =(job)
+#         @job = job
+#     end
+# end
+
+class Person
+    def name
+      @name
+    end
+  
+    def name=(name)
+      @name = name
+    end
+  
+    def job
+      @job
+    end
+  
+    def job=(job)
+      @job = job
+    end
+  end
